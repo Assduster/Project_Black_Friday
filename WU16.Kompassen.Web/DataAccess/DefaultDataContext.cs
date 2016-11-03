e394bf770815a5be42271568808b86c6615d281d
David Hallberg <heda1616@student.ju.se>
2016-11-03 13:07:18 +0100

    Project Black Friday

---
+++ WU16.Kompassen.Web/DataAccess/DefaultDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using WU16.Kompassen.Web.Models;
+
+namespace WU16.Kompassen.Web.DataAccess
+{
+    public class DefaultDataContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
