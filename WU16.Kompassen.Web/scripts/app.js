53146b7b5dd4028fa359427eaea0bf055825261d
Rarex <Rarex@RAREX-DATOR>
2016-11-09 21:44:48 +0100

    Daniel: Städade upp lite i koden och la till kommentarer

--- WU16.Kompassen.Web/scripts/app.js
+++ WU16.Kompassen.Web/scripts/app.js
@@ -1,23 +1,31 @@
-﻿var url = "http://localhost:45959/";
+﻿var url = "http://localhost:45959/"; //satt som global för att fungera i alla GET/POST. Ändra efter behov.
 
 var app = {
+
+    //Searchquery
     searchForStudent: function (searchQuery) {
-        //Searchquery
         $.ajax({
             type: "GET",
             url: url + "api/searchStudents/" + searchQuery,
         }).done(function (data) {
             console.log(data);
             for (var counter = 0; counter < data.length; counter++) {
-                $("#studentListTable > tbody").append("<tr><td>" + data[counter].firstName + "</td><td>" + data[counter].lastName + "</td><td>" + data[counter].ssn + "</td><td>" + listOfCourses(data[counter]) + "</td></tr>");
+                $("#studentListTable > tbody").append("<tr><td>" +
+                                            data[counter].firstName + "</td><td>" +
+                                            data[counter].lastName + "</td><td>" +
+                                            data[counter].ssn + "</td><td>" +
+                                            listOfCourses(data[counter]) + "</td></tr>"); //Kallar funktion för att plocka ut studentens kurser.
+
                 function listOfCourses(data) {
                     var results = [];
-                    for (var i = 0; i < data.courses.length; i++) {
-                        results.push(data.courses[i].name);
+                    for (var counter = 0; counter < data.courses.length; counter++) {
+                        results.push(data.courses[counter].name);
                     }
                     return results;
                 }
             }
+        }).fail(function () {
+            console.log("Error!"); //TODO 
         });
     }
 }
