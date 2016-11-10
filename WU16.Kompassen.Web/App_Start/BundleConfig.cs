9c9a1be4f515fb36ec2524239f275286c7a6d1d0
David Hallberg <heda1616@student.ju.se>
2016-11-10 12:17:47 +0100

    Skapade mapp för våra js-filer

--- WU16.Kompassen.Web/App_Start/BundleConfig.cs
+++ WU16.Kompassen.Web/App_Start/BundleConfig.cs
@@ -16,9 +16,9 @@ namespace WU16.Kompassen.Web
                         "~/scripts/jquery.easing*"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                        "~/scripts/app.js",
-                        "~/scripts/students.js",
-                        "~/scripts/courses.js"));
+                        "~/scripts/sitecore/app.js",
+                        "~/scripts/sitecore/students.js",
+                        "~/scripts/sitecore/courses.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
