7dc744136467898350d4e28bb228f390aafd6f3e
David Hallberg <heda1616@student.ju.se>
2016-11-15 09:04:01 +0100

    Rensade lite smått. Gjorde så man kan sortera kurser och studenter. Bokstavsordning och nummerordning.

--- WU16.Kompassen.Web/scripts/sitecore/init.js
+++ WU16.Kompassen.Web/scripts/sitecore/init.js
@@ -14,10 +14,12 @@
                 case "#students":    
                     students.studentVisibility();
                     students.listOfStudents();
+                    students.sortStudents();
                     break;
                 case "#courses":
                     courses.courseVisibility();
                     courses.listAllCourses();
+                    courses.sortCourses();
                     break;
                 case "#addCourse":
                     courses.createCourseVisibility();
